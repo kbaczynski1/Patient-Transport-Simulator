cef27d982c37456ce7ba5f29e47fa382bf825963
rvbc1 <marek_knosala@vp.pl>
2020-12-19 21:27:48 +0100

    javaFX preset windows & linux

---
+++ src/controllers/MainWindowController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+
+
+import application.MainGUI;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+
+public class MainWindowController {
+    @FXML
+    void initialize() {
+    }
+}
