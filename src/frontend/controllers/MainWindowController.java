fdb2f89eda91c526fa906808551165fe6ca77253
rvbc1 <marek_knosala@vp.pl>
2021-01-05 17:30:19 +0100

    gui update

--- src/frontend/controllers/MainWindowController.java
+++ src/frontend/controllers/MainWindowController.java
@@ -19,21 +19,21 @@ public class MainWindowController {
 
     public void doActionsAfterLoadMap(){
         hospitalsWindowController.updateHospitalTable();
-        Thread readingThread = new Thread() {
-            public void run() {
-                int free = 0;
-                while(true) {
-                    DataBase.getHospital(1).setFreeBedsAmount(free);
-                    free++;
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        readingThread.setDaemon(true);
-        readingThread.start();
+//        Thread readingThread = new Thread() {
+//            public void run() {
+//                int free = 0;
+//                while(true) {
+//                    DataBase.getHospital(1).setFreeBedsAmount(free);
+//                    free++;
+//                    try {
+//                        Thread.sleep(100);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        };
+//        readingThread.setDaemon(true);
+//        readingThread.start();
     }
 }
